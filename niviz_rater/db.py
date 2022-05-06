ac0e66c856a744ec6c3bd3d54554e3710a61279e
Dawn Smith <dawn.smith@camh.ca>
2022-05-06 15:30:32 -0400

    [ENH] Allow postgresql database to be used

--- niviz_rater/db.py
+++ niviz_rater/db.py
@@ -1,11 +1,20 @@
-from peewee import SqliteDatabase
-from typing import Any, List, Optional, Callable
+import os
+
+from peewee import Database, SqliteDatabase, PostgresqlDatabase
+from typing import Any, List, Dict, Optional, Callable
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
 def get_or_create_db(
-        db_str: str,
-        additional_pragmas: Optional[List[Any]] = None) -> SqliteDatabase:
+        app_config: Dict[str, str],
+        additional_pragmas: Optional[List[Any]] = None) -> Database:
 
+    if 'datman_config' in app_config:
+        return create_postgres_db(app_config)
+
+    db_str = app_config['niviz_rater.db.file']
     pragmas = [('foreign_keys', 'on')]
     if additional_pragmas:
         pragmas.append(additional_pragmas)
@@ -24,5 +33,28 @@ def fetch_db_from_config(app_config,
 
     return app_config.get(
         'niviz_rater.db.instance',
-        get_or_create_db(app_config['niviz_rater.db.file'],
+        get_or_create_db(app_config,
                          additional_pragmas))
+
+
+def create_postgres_db(app_config: Dict[str, str]) -> PostgresqlDatabase:
+    dm_config = app_config['datman_config']
+    name = dm_config['db_name']
+    user = os.getenv(dm_config['user'])
+    password = os.getenv(dm_config['password'])
+    host = os.getenv(dm_config['server'])
+
+    connection = psycopg2.connect(
+        dbname='postgres',
+        user=user,
+        host=host,
+        password=password)
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(sql.SQL(f'CREATE DATABASE {name}'))
+    except psycopg2.errors.DuplicateDatabase:
+        pass
+
+    return PostgresqlDatabase(name, user=user, password=password, host=host)
